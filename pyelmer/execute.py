165c273e2e435e2e6002f00f0b5b2e37e07efc27
Mudassir Chapra <37051110+muddi900@users.noreply.github.com>
2023-01-05 10:51:57 +0000

    added bool for keeping the newly created directory

--- pyelmer/execute.py
+++ pyelmer/execute.py
@@ -16,6 +16,7 @@ def run_elmer_grid(sim_dir, meshfile, elmergrid=None, **kwargs):
         elmergrid (str): ElmerGrid executable
     Kwargs:
         out_dir (str): Optional directory to save the output.
+        keep_mesh_dir(bool): Whether to keep the mesh directory
     """
     if elmergrid is None:
         # On Windows ElmerGrid.exe is not found once gmsh.initialize() was executed.
@@ -30,6 +31,10 @@ def run_elmer_grid(sim_dir, meshfile, elmergrid=None, **kwargs):
         subprocess.run(args, cwd=sim_dir, stdout=f, stderr=f)
 
     mesh_dir = sim_dir + "/" + ".".join(meshfile.split(".")[:-1])
+    keep_mesh_dir = kwargs.get('keep_mesh_dir', False)
+    if keep_mesh_dir:
+        return None
+
     out_dir = kwargs.get('out_dir', sim_dir)
     files = os.listdir(mesh_dir)
     for f in files:
